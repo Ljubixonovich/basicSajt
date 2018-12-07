0c20a59049edb974b956e213509aa036e9db99c8
Ljubomir <ljubomir.vukelic@gmail.com>
2018-12-07 12:47:03 +0100

    bla

---
+++ lj.js
@@ -0,0 +1,19 @@
+(function (lj) {
+    lj.author = {},
+    lj.deca = [
+        {sinko: {
+            name: 'Viktor',
+            age: 6
+        }}
+    ],
+    lj.zena = {
+        name: 'Jasna',
+        age: 42
+    },
+    lj.knowledge = [
+        'Psychology',
+        'IT',
+        'Wisdom'
+    ],
+    lj.married = 123
+}(window.lj = window.lj || {}));
--- script.js
+++ script.js
@@ -2,48 +2,19 @@
 'use strict';
 
 $(document).ready(function() {
-	// alert(11);
 	
-	var persons = [
-		{
-			name: 'joca',
-			age: 15,
-			smart: false
-		},
-		{
-			name: 'pera',
-			age: 22,
-			smart: true
-		}
-	];
-	var p1 = { name: 'ljuba', age: 36, smart: true};
-	var p2 = { name: 'milos', age: 46, smart: false};
-	
-	var p3 = {...p1, ...p2};
-	
-	
-	var persons2 = [...persons, p3];
-	
-	
-	
-	
-	
-	
-	for (let p of persons2) {		 
-		console.log(p.name + ' has ' + p.age + ' and is ' + (p.smart === true ? 'smart' : 'stupid'));
-	}
-	
-	
-	
-	
+	var result = lj.knowledge[1];
+	console.log(result);
+
+	lj.knowledge.push('Water');
+	console.log(lj.knowledge);
 	
 	
     $("#p1").css("font-size", "50px");
 	$("p").click(function() {
 		$(this).hide();		
 	});
+
+	
+
 });
-
-
-
-
