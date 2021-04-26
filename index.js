5f286d9c6afbd88d22f48c8cabcb6a24af295779
Ljubixonovich <ljubomir.vukelic@gmail.com>
2021-04-26 10:36:57 +0200

    defer atribute added

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+log = (...logs) => console.log(...logs)
 
-
-const btn1 = document.getElementById('btn1');
-const btn2 = document.getElementById('btn2');
-log = (...logs) => console.log(...logs);
-
-
-btn1.onclick = function() {
-	getPosts()
-	.then(p => {
-		const posts = p;
-		log('posts: ', posts)
-		return
-	})
-	.then(() => log('na kraju'))
-	.then(() => log('na kraju 2'))
-	.catch(error => log('error: ', error))
+btn1.onclick = function () {
+  getPosts()
+    .then((p) => {
+      const posts = p
+      log('posts: ', posts)
+      return
+    })
+    .then(() => log('na kraju'))
+    .then(() => log('na kraju 2'))
+    .catch((error) => log('error: ', error))
 }
 
 // btn1.onclick = async function() {
 // 	try {
 // 		const posts = await getPosts()
-// 		console.log('posts: ', posts)		
+// 		console.log('posts: ', posts)
 // 	} catch (error) {
-// 		console.log('error: ', error)		
+// 		console.log('error: ', error)
 // 	}
 // }
 
@@ -34,12 +31,12 @@ btn1.onclick = function() {
 // 	.catch(error => log('error: ', error))
 // }
 
-btn2.onclick = async e =>  {
-	console.log('e: ', e)
-	try {
-		const p = await getPosition()
-		log('Position: ', p.coords)		
-	} catch (error) {
-		log(error)
-	}
+btn2.onclick = async (e) => {
+  console.log('e: ', e)
+  try {
+    const p = await getPosition()
+    log('Position: ', p.coords)
+  } catch (error) {
+    log(error)
+  }
 }
